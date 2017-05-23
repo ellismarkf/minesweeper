a057d079f0323cb99edf351f202437e392b5ec52
Mark Ellis <ellismarkf@gmail.com>
2017-05-22 22:36:42 -0500

    ✨ - handle right clicks; set game state on game over

--- src/components/tile/index.js
+++ src/components/tile/index.js
@@ -1,19 +1,26 @@
 import { linkEvent } from 'inferno';
-import { iterativeSweep, swept } from '../../lib/minesweeper';
+import { iterativeSweep, swept, safe, won, lost, flagTile, flagged } from '../../lib/minesweeper';
 
 const tileContent = {
   0: '',
   1: '',
   2: '',
   3: '💣',
-  4: '🚩'
+  4: '🚩',
+  5: '🚩',
+  6: '🚩',
+  7: '💣'
 }
 
 const tileStyle = {
   0: 'tile',
   1: 'tile',
   2: 'swept-tile',
-  3: 'swept-mine'
+  3: 'swept-mine',
+  4: 'flagged-tile',
+  5: 'flagged-tile',
+  6: 'flagged-swept-tile',
+  7: 'flagged-swept-mine'
 }
 
 function generateContent(value, threats) {
@@ -32,12 +39,32 @@ function generateClassList(value, threats) {
   return classList;
 }
 
-function handleClick(props) {
+function isRightClick(type) {
+  return type === 2;
+}
+
+function handleClick(props, event) {
+  const { button: clickType } = event;
   const { pos, context: { data: context }} = props;
+  if (context.state.game & lost || context.state.game & won) return;
+  if (isRightClick(clickType)) {
+    if (context.state.tiles[pos] & swept) return;
+    const newTiles = flagTile(pos, context.state.tiles);
+    context.setState({
+      tiles: newTiles,
+    });
+    return false;
+  }
+  if (context.state.tiles[pos] & flagged) return;
   const newTiles = iterativeSweep(pos, context.state.tiles, context.state.threats, context.state.cols);
-  context.setState({
-    tiles: newTiles,
+  const notMine = safe(newTiles);
+  context.setState(function(prevState) {
+    return {
+      tiles: newTiles,
+      game: notMine ? prevState.game : lost,
+    }
   });
+  return false;
 };
 
 export function shouldUpdate(lastProps, nextProps) {
@@ -49,7 +76,7 @@ export default function Tile(props) {
   return (
     <div
       className={generateClassList(value, threats)}
-      onClick={linkEvent(props, handleClick)}
+      onMouseDown={linkEvent(props, handleClick)}
       noNormalize
     >
       <span>{generateContent(value, threats)}</span>
