0bfbdd52cff11b5b73d8d338992b0a653b584e01
Mark Ellis <ellismarkf@gmail.com>
2017-06-09 16:52:25 -0500

    ✨ - load and play minefields from the server

--- src/pages/play/index.js
+++ src/pages/play/index.js
@@ -1,5 +1,6 @@
 import Component from 'inferno-component';
 import Minesweeper from '../../components/minesweeper';
+import { board } from '../../lib/minesweeper';
 import './play.css';
 
 const difficulties = [
@@ -26,26 +27,65 @@ const difficulties = [
 ];
 
 export default class Play extends Component {
+
+  state = {
+    rows: 9,
+    cols: 9,
+    mines: 10,
+    tiles: [],
+    loading: false,
+  }
+
   componentDidMount() {
     const { gameId } = this.props.params;
     const id = parseInt(gameId, 10);
     if (id < 4) return;
+    this.setState({
+      loading: true,
+    });
     window.fetch(
       `https://minesweeper-backend-api.herokuapp.com/minefields/${id}`
     )
     .then(res => res.json())
     .then(result => {
-      console.log(result);
+      this.setState({
+        rows: result.rows,
+        cols: result.cols,
+        mines: result.mines,
+        tiles: result.tiles,
+        loading: false,
+      });
+    })
+    .catch(err => {
+      console.log(err.message, err.stack);
+      this.setState(function(prevState) {
+        return {
+          ...board(prevState.rows, prevState.cols, prevState.mines),
+          loading: false,
+        }
+      });
     });
   }
 
   render() {
     const { gameId } = this.props.params;
     const id = parseInt(gameId, 10);
-    const props = difficulties[id];
+    if (id < 4) {
+      const props = difficulties[id];
+      return (
+        <div className="play-page-container">
+          <Minesweeper {...props} />
+        </div>
+      );
+    }
+    if (this.state.loading) {
+      return (
+        <h1>...Loading your game</h1>
+      )
+    }
     return (
       <div className="play-page-container">
-        <Minesweeper {...props} />
+        <Minesweeper {...this.state} />
       </div>
     );
   }
