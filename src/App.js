e9b8e05863e3824635377fc65d9011d84e938c04
Mark Ellis <ellismarkf@gmail.com>
2017-06-11 20:06:32 -0500

    💄 - add site-wide link to home page closes #58

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Component from 'inferno-component';
+import { Link } from 'inferno-router';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Link to="/">🏠 Home</Link>
         { this.props.children } 
       </div>
     );
